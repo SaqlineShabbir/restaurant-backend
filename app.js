b1a67be351a03aecefd3a3362dda448a59dcc19b
SaqlineShabbir <saqline5869@gmail.com>
2024-02-12 11:31:41 +0600

    changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //
 // extrernal middlewares
 const corsOptions = {
-  origin: '*',
+  origin: ['http://localhost:5173', 'https://restaurant-sage-theta.vercel.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-})
+});
 
 
 // Cloudinary
