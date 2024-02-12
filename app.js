9e316971c6e4e8a912152406ac7ecb5c293fc0f0
SaqlineShabbir <saqline5869@gmail.com>
2024-02-12 11:54:11 +0600

    changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 //
 // extrernal middlewares
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://restaurant-sage-theta.vercel.app'],
+  origin: '*',
   credentials: true,
   optionSuccessStatus: 200,
 }
@@ -23,26 +23,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handling preflight requests
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    ["http://localhost:5173", "https://restaurant-sage-theta.vercel.app"]
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+})
 
 
 // Cloudinary
