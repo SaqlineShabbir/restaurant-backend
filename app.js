fed7bff4bbab4a94027ecd14000df1c14295bb31
SaqlineShabbir <saqline5869@gmail.com>
2024-02-12 11:37:02 +0600

    fixing corss   origin  error

--- app.js
+++ app.js
@@ -14,18 +14,33 @@ app.use(express.json());
 app.use(cookieParser());
 //
 // extrernal middlewares
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://restaurant-sage-theta.vercel.app'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//   origin: ['http://localhost:5173', 'https://restaurant-sage-theta.vercel.app'],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Handling preflight requests
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
   next();
 });
 
