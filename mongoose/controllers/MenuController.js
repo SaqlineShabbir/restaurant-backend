43b93cbae201082ee2bc3761f12c20c1903e6d3a
SaqlineShabbir <saqline5869@gmail.com>
2024-06-22 21:10:19 +0600

    update menu added

--- mongoose/controllers/MenuController.js
+++ mongoose/controllers/MenuController.js
@@ -1,15 +1,15 @@
-const Menu = require('../models/Menu');
-const cloudinary = require('cloudinary')
+const Menu = require("../models/Menu");
+const cloudinary = require("cloudinary");
 exports.getMenus = async (req, res, next) => {
   try {
     const menus = await Menu.find({});
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: menus,
     });
   } catch (err) {
     res.status(400).json({
-      status: 'Failed',
+      status: "Failed",
       message: err.message,
     });
   }
@@ -17,11 +17,10 @@ exports.getMenus = async (req, res, next) => {
 
 exports.postMenu = async (req, res, next) => {
   try {
-
-    const { name, description, price, category, quantity, status } = req.body
+    const { name, description, price, category, quantity, status } = req.body;
     const file = req.file.path;
     const cloud = await cloudinary.uploader.upload(file);
-    console.log(file)
+    console.log(file);
     const result = await Menu.create({
       name,
       description,
@@ -29,17 +28,17 @@ exports.postMenu = async (req, res, next) => {
       category,
       quantity,
       status,
-      photo: cloud?.url
+      photo: cloud?.url,
     });
-
+    console.log(result);
 
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: result,
     });
   } catch (err) {
     res.status(400).json({
-      status: 'Failed',
+      status: "Failed",
       message: err.message,
     });
   }
@@ -49,12 +48,12 @@ exports.getMenuById = async (req, res, next) => {
   try {
     const menu = await Menu.findOne({ _id: id });
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: menu,
     });
   } catch (err) {
     res.status(400).json({
-      status: 'Failed',
+      status: "Failed",
       message: err.message,
     });
   }
@@ -64,12 +63,36 @@ exports.deleteMenu = async (req, res, next) => {
   try {
     const deletemenu = await Menu.findByIdAndDelete({ _id: id });
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: deletemenu,
     });
   } catch (err) {
     res.status(400).json({
-      status: 'Failed',
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.updateMenu = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, description, price, category } = req.body;
+
+  try {
+    const updatedData = { name, description, price, category };
+
+    const updatedMenu = await Menu.findByIdAndUpdate(id, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: updatedMenu,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
       message: err.message,
     });
   }
--- mongoose/routes/MenuRoute.js
+++ mongoose/routes/MenuRoute.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const menuController = require('../controllers/MenuController');
-const requireAuth = require('../middlewares/AuthMiddleWare');
-const upload = require('../../middlewares/FileUpload');
-
+const menuController = require("../controllers/MenuController");
+const requireAuth = require("../middlewares/AuthMiddleWare");
+const upload = require("../../middlewares/FileUpload");
 
 router
-  .route('/')
-  .post(upload.single('photo'), menuController.postMenu)
+  .route("/")
+  .post(upload.single("photo"), menuController.postMenu)
   .get(menuController.getMenus);
-router.route('/:id').get(menuController.getMenuById);
-router.route('/:id').delete(menuController.deleteMenu);
+router.route("/:id").get(menuController.getMenuById);
+router.route("/:id").delete(menuController.deleteMenu);
+router.route("/:id").put(menuController.updateMenu);
 
 module.exports = router;
